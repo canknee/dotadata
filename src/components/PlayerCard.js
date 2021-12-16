5298fbbfed46717fe848eb7f0794f5d97b3f79b4
Nathaniel Scholnick <nate.scholnick@gmail.com>
2021-12-16 09:56:53 -0500

    styling(player components): added card classes and css

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import WinLoss from './WinLoss'
 import './PlayerCard.css';
 
-const PlayerCard = ({ player, winloss }) => {
+const PlayerCard = ({ player, winLoss }) => {
   if (!player) return null;
   console.log(player)
   return (
     <div className="playerInfo">
-      <img src={player.profile.avatarmedium} alt="player avatar" />
+      <img className="playerAvatar" src={player.profile.avatarfull} alt="player avatar" />
       <a target="_blank" rel="noreferrer" href={player.profile.profileurl} className="playerName">
-        <h2>{player.profile.personaname}</h2>
+        <h2 className="playerName">{player.profile.personaname}</h2>
       </a>
       <p className="mmr">MMR: {player.mmr_estimate.estimate}</p>
+      <WinLoss winLoss={winLoss} />
     </div>
   )
 }
--- src/components/WinLoss.js
+++ src/components/WinLoss.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './WinLoss.css'
 
-const WinLoss = ({ winloss }) => {
-    if (!winloss) return null;
-    console.log(winloss)
+const WinLoss = ({ winLoss }) => {
+    if (!winLoss) return null;
     return (
-    <div className="winloss">
-        {` Wins: ${winloss.win}  Loss: ${winloss.lose} Win Rate: ${(winloss.win/(winloss.win + winloss.lose)).toFixed(4)*100}%`}
+    <div className="winLoss">
+        W: <span className="winCount">{winLoss.win}</span>  L: <span className="lossCount">{winLoss.lose}</span>, Win Rate: {(winLoss.win/(winLoss.win + winLoss.lose)*100).toFixed(2)}%
     </div>
     )
   }
