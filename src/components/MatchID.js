d871f43f1959c82fb67908bac424ea08fcec9919
Nathaniel Scholnick <nate.scholnick@gmail.com>
2021-12-17 02:12:08 -0500

    added react bootstrap table for match results

--- src/components/MatchID.js
+++ src/components/MatchID.js
@@ -1,38 +1,43 @@
 import React from 'react';
-import './MatchId.css'
-import heroList from '../helpers/HeroList';
-// Think about doing this with a table from React bootstrap
+import TableTeamResults from './TableTeamResults';
+import './MatchId.css';
+import { Table } from 'react-bootstrap';
 
-const MatchId = ({ match, heroes }) => {
+const MatchId = ({ match }) => {
     if (!match) return null;
-    var minutes = Math.floor(match.duration / 60);
-    var seconds = ('0' + match.duration % 60).slice(-2)
-
+    const minutes = Math.floor(match.duration / 60);
+    const seconds = ('0' + match.duration % 60).slice(-2);
+    const winner = match.radiant_win ? 'Radiant' : 'Dire';
+    const playersRadiant = match.players.filter(player => player.player_slot <= 127);
+    const playersDire = match.players.filter(player => player.player_slot > 127);
+    
     return (
         <div>
             <h3>Match Details</h3>
             <div className="fullMatchCard">
-                <div className='match'>
-                    Match ID: <a target="_blank" rel="noreferrer" href={`https://www.opendota.com/matches/${match.match_id}`} className="match"> {match.match_id} </a>
-                </div>
-                <div className="matchdata">
-                    {` ${match.radiant_win ? 'Radiant Victory' : 'Dire Victory'} | Game length: ${minutes + ':' + seconds} | Region: ${match.region===1 ? 'USW' : 'USE'} | 
-                    Skill level: ${ (match.skill===1) ? ('Normal skill') : ((match.skill===2) ? ('High skill') : ('Very High Skill'))}  `}
-                </div>
-                <div className="score">
-                    {` Radiant: ${match.radiant_score} Dire: ${match.dire_score} `}
-                </div>
-                {match.players.map(({personaname, kills, deaths, assists, hero_id, last_hits, denies, player_slot}) => {
-                    var team = '';
-                    if (player_slot <= 127) team = 'Radiant';
-                    if (player_slot >= 128) team = 'Dire';
-                    return (
-                        <div className="players">
-                            {`${personaname} | ${team} Hero: ${heroList.find((hero) => hero.id===hero_id).localized_name} | Kills: ${kills}, Death: ${deaths}, Assists: ${assists} LH: ${last_hits}, Denies: ${denies}`}
+                <div className="headerSection">
+                    <div className="matchWinnerWrapper">
+                        <div className={"matchWinner " + winner}>
+                            {winner + ' Victory'}
                         </div>
-                    )
-                })}
-            </div>   
+                    </div>
+                    <div className="matchScore">
+                        <span className="Radiant">{match.radiant_score}</span> - <span className="Dire">{match.dire_score}</span>
+                        <div className="matchLength">
+                            {minutes + ':' + seconds}
+                        </div>
+                    </div>
+                    <div className="matchGeneralInfo">
+                        ID: <a target="_blank" rel="noreferrer" href={`https://www.opendota.com/matches/${match.match_id}`} className="match">{match.match_id}</a>{' '}
+                        / {match.region===1 ? ' USW ' : ' USE '}
+                        / {(match.skill===1) ? (` Normal${'\u00A0'}skill`) : ((match.skill===2) ? (` High${'\u00A0'}skill`) : (` Very${'\u00A0'}High${'\u00A0'}Skill`))}
+                    </div>
+                </div>
+                <div className="tableSection">
+                    <TableTeamResults players={playersRadiant} tableTitle="Radiant" />
+                    <TableTeamResults players={playersDire} tableTitle="Dire" />
+                </div>
+            </div>
         </div>
     )
   }
